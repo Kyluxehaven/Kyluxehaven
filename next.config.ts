9da86a7d886c06167d55684dba0c026f4e2dbe2c
Patrick Marcus <patrickmarcus6363@gmail.com>
2025-09-11 17:45:49 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -39,6 +39,12 @@ const nextConfig: NextConfig = {
         hostname: 'encrypted-tbn0.gstatic.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'content.josephturner.co.uk',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
